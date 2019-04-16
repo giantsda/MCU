c51a119f3f0ad459c05f48b67d0ba24a8cfceca6
giantsda <chenshen@rams.colostate.edu>
2019-04-16 10:48:20 -0600

    Done: modified flowMeterCalibration.cpp so that I can redirect two serial communication into one file to make curve fitting easier.

--- flowMeter/ESP32/flowMeterCalibration.cpp
+++ flowMeter/ESP32/flowMeterCalibration.cpp
@@ -1,6 +1,3 @@
-
-// One run for different velocity code with fine intervals. It will produce one file. The velocity is predefined in the code.
-
 #include <chenStepper.h>
 const int ena = 4;
 const int dir = 5;
@@ -19,11 +16,13 @@ chenStepper stepper;
 
 void
 startMeasure ();
+
 int runTime=1;
 
 void
 setup ()
 {
+
   Serial.begin (115200);
   pinMode (leftLimitPin, INPUT_PULLDOWN);  // sets the digital pin as input
   pinMode (startPin, INPUT_PULLUP);
@@ -38,7 +37,7 @@ setup ()
     digitalWrite (pul, LOW);
     delayMicroseconds (700);
   }
-  Serial.println ("in position");
+  Serial.println ("Stepper in position");
 
   digitalWrite (dir, HIGH); // go forward
   delay (200);   
@@ -55,25 +54,30 @@ delay (10000);
  for (double velocity=0.05;velocity<1.5;velocity=velocity+0.0125)
   {
     low=149.8445/velocity;
-     delay (3000);
-    Serial.println ("RUN");
+    delay (3000);
+    Serial.print ("Forward start. Velocity= ");
     Serial.println (velocity,5);
-    Serial.println (low);
     steps1 = stepper.findAccelerationSteps (high, low);
     delayArray = (int*) malloc (sizeof(int) * steps1);
     stepper.getDelayArray (high, low, steps1, delayArray);
-    Serial.println (steps1);
     stepper.Accelerate (delayArray, steps1);
     stepper.moveI (low, totalLength - steps1 * 2);
     stepper.Deaccelerate (delayArray, steps1);
+    Serial.print ("Forward end. Velocity= ");;
+    Serial.println (velocity,5);
     free(delayArray);
-     
+      
+
 // go back
     digitalWrite (dir, LOW);
     delay (5000);
+    Serial.println ("Back start");
     stepper.moveI (700, totalLength);
+    Serial.println ("Back end");
     digitalWrite (dir, HIGH);
   }
+
+
   while(1)
   ;
 }
