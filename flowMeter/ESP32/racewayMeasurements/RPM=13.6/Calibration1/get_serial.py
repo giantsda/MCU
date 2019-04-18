e10b3776c7fdf8bdaf8d54e7704c21f65f2c9fd4
giantsda <chenshen@rams.colostate.edu>
2019-04-18 14:36:25 -0600

    added racewayMeasurements

---
+++ flowMeter/ESP32/racewayMeasurements/RPM=13.6/Calibration1/get_serial.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import serial 
+import sys
+
+argLen=len(sys.argv)
+if argLen!=3:
+	port='/dev/ttyACM0'  # default port
+	baudrate='9600'
+else:
+	port=sys.argv[1]
+	baudrate=sys.argv[2]
+
+ser= serial.Serial(port,baudrate)
+i=0    
+while(ser.isOpen()):
+	# print(ser.isOpen())
+	# print(ser.read(82).decode('utf-8'),end="", flush=True)
+	line=ser.readline()
+	line=line.decode('utf-8')
+	if i>=0:
+		print(line,end="", flush=True)   # start to wrtie data from the third line
+	i=i+1	
+
+
+ 
